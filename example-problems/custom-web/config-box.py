0089ff11ff467c3a05c5aceecf0bcb7a775630d1
syreal17 <ltjones@acm.org>
2021-07-30 15:01:01 -0400

    Remove unused imports

--- example-problems/custom-web/config-box.py
+++ example-problems/custom-web/config-box.py
@@ -5,55 +5,47 @@
 
 import sys
 import os
-import subprocess
 import re
-import zlib
 import json
 
 
 
 def main():
 
-    try:
-        # Split flag into 3 parts  ============================================
-        flag = os.environ.get("FLAG")
+    # picofy flag and append to stylesheet ================================
+    flag = os.environ.get("FLAG")
 
-        if flag == "":
-            print("Flag was not read from environment. Aborting.")
-            sys.exit(-1)
+    if flag == "":
+        print("Flag was not read from environment. Aborting.")
+        sys.exit(-1)
+    else:
+        # Get hash part
+        flag_rand = re.search("{.*}$", flag)
+        if flag_rand == None:
+            print("Flag isn't wrapped by curly braces. Aborting.")
+            sys.exit(-2)
         else:
-            # Get hash part
-            flag_rand = re.search("{.*}$", flag)
-            if flag_rand == None:
-                print("Flag isn't wrapped by curly braces. Aborting.")
-                sys.exit(-2)
-            else:
-                flag_rand = flag_rand.group()
-                flag_rand = flag_rand[1:-1]
+            flag_rand = flag_rand.group()
+            flag_rand = flag_rand[1:-1]
 
-        new_flag = "picoCTF{1n5p3t0r_ftw_" + flag_rand + "}"
-        
-        with open("/usr/local/apache2/htdocs/style.css", "a") as f:
-            f.write('/* ' + new_flag + ' */')
+    new_flag = "picoCTF{1n5p3t0r_ftw_" + flag_rand + "}"
+    
+    with open("/usr/local/apache2/htdocs/style.css", "a") as f:
+        f.write('/* ' + new_flag + ' */')
 
-        # =====================================================================
+    # =====================================================================
 
 
-        # Create and update metadata.json =====================================
+    # Create and update metadata.json =====================================
 
-        metadata = {}
-        metadata['flag'] = str(new_flag)
-        json_metadata = json.dumps(metadata)
-        
-        with open("/challenge/metadata.json", "w") as f:
-            f.write(json_metadata)
+    metadata = {}
+    metadata['flag'] = str(new_flag)
+    json_metadata = json.dumps(metadata)
+    
+    with open("/challenge/metadata.json", "w") as f:
+        f.write(json_metadata)
 
-        # =====================================================================
-
-    except subprocess.CalledProcessError:
-        print("A subprocess has returned an error code")
-        sys.exit(1)
-
+    # =====================================================================
 
 
 # =============================================================================
